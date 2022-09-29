85b0a3b40f7fac837fe95ffe5eb1bf182c4f08c8
Diego Sano <diego.sano@nave.rs>
2022-09-29 15:53:06 -0300

    initial commit

---
+++ src/simpleServer.ts
@@ -0,0 +1,48 @@
+import { ApolloServer, gql } from 'apollo-server'
+import { randomUUID } from 'node:crypto'
+
+interface User {
+  id: string
+  name: string
+}
+
+const users: User[] = []
+
+const typeDefs = gql`
+  type User {
+    id: String!
+    name: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): User!
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      users: () => users,
+    },
+
+    Mutation: {
+      createUser: (_, { name }) => {
+        const user = {
+          id: randomUUID(),
+          name,
+        }
+        users.push(user)
+        return user
+      }
+    }
+  }
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
